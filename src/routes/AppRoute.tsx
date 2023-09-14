1c048f9dd349f835cc700bbd15f25412ac604cf5
yryskeldi0101 <yryskeldi.9121@gmail.com>
2023-09-14 17:38:04 +0600

    created

---
+++ src/routes/AppRoute.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppRoute = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppRoute
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Graphik', 'Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: false,
+    utils: false,
+    logs: false,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'light',
+  },
+}
