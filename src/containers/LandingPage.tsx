32503aafa5099cbaabeaaf0e13761032c979b182
Begimai7 <begikeldibekova@gmail.com>
2023-09-16 04:24:30 +0600

    processing

--- src/containers/LandingPage.tsx
+++ src/containers/LandingPage.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import { Header } from "../components/header/Header";
 import { MainSlider } from "../components/mainSlider/MainSlider";
 import { CompanyCards } from "../components/companyCards/CompanyCards";
-import { Footer } from "../components/footer/Footer";
 
 function LandingPage() {
   const [openCompanyCard, setOpenCompanyCard] = useState(false);
@@ -16,22 +14,14 @@ function LandingPage() {
   const openSignUpBlockHandler = () => {
     setOpenCompanyCard(true);
   };
-  const openMessageHandler = () => {
-    setOpenCompanyCard(true);
-  };
 
   return (
     <div className=" bg-gradient-to-r from-white-50 to-fuchsia-50">
-      <Header
-        openSignUpBlockHandler={openSignUpBlockHandler}
-        openMessageHandler={openMessageHandler}
-      />
       <MainSlider openSignUpBlockHandler={openSignUpBlockHandler} />
       <CompanyCards
         openCompanyCardHandler={openCompanyCardHandler}
         openAllCompaniesCard={openAllCompaniesCard}
       />
-      <Footer />
     </div>
   );
 }
--- src/routes/GuestRoutes.tsx
+++ src/routes/GuestRoutes.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import GuestLayout from '../layout/GuestLayout'
-import LandingPage from '../containers/LandingPage'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import GuestLayout from "../layout/GuestLayout";
+import LandingPage from "../containers/LandingPage";
 
 const GuestRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<GuestLayout/>}>
-        <Route index element={<LandingPage/>}/>
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/" element={<GuestLayout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="all_componies" element={<LandingPage />} />
+        <Route path="about" element={<LandingPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default GuestRoutes
+export default GuestRoutes;
