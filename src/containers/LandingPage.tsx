165edbdf5eea1c1e476da2bce0a4b4451212fb91
Begimai7 <begikeldibekova@gmail.com>
2023-09-16 04:26:46 +0600

    processing

---
+++ src/containers/LandingPage.tsx
@@ -0,0 +1,29 @@
+// import React, { useState } from "react";
+// import { MainSlider } from "../components/mainSlider/MainSlider";
+// import { CompanyCards } from "../components/companyCards/CompanyCards";
+
+// function LandingPage() {
+//   const [openCompanyCard, setOpenCompanyCard] = useState(false);
+
+//   const openCompanyCardHandler = () => {
+//     setOpenCompanyCard(true);
+//   };
+//   const openAllCompaniesCard = () => {
+//     setOpenCompanyCard(true);
+//   };
+//   const openSignUpBlockHandler = () => {
+//     setOpenCompanyCard(true);
+//   };
+
+//   return (
+//     <div className=" bg-gradient-to-r from-white-50 to-fuchsia-50">
+//       <MainSlider openSignUpBlockHandler={openSignUpBlockHandler} />
+//       <CompanyCards
+//         openCompanyCardHandler={openCompanyCardHandler}
+//         openAllCompaniesCard={openAllCompaniesCard}
+//       />
+//     </div>
+//   );
+// }
+
+// export default LandingPage;
