edc212b2005780796505afd817fd37913704012a
FrontenderO07 <nurlanbekovich2006@gmail.com>
2023-09-16 22:09:07 +0600

    finished

--- src/components/form/SignUpPage.tsx
+++ src/components/form/SignUpPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as z from "zod";
-import { SubmitHandler, useForm, Controller } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "react-router-dom";
 import clsx from "clsx";
